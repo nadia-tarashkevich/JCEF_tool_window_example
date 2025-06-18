3bbc36bacaa7d698eefb629c94660043d9b1aae9
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2025-06-17 18:28:56 -0700

    Added service tags

--- src/test/kotlin/org/jetbrains/plugins/template/MyPluginTest.kt
+++ src/test/kotlin/org/jetbrains/plugins/template/MyPluginTest.kt
@@ -1,12 +1,14 @@
 package org.jetbrains.plugins.template
 
 import com.intellij.ide.highlighter.XmlFileType
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
 import org.jetbrains.plugins.template.services.MyProjectService
+import org.jetbrains.plugins.template.services.MyService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -33,6 +35,12 @@ class MyPluginTest : BasePlatformTestCase() {
         val projectService = project.service<MyProjectService>()
 
         assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
+    }
+
+    fun testMyService() {
+        val myService = ApplicationManager.getApplication().service<MyService>()
+
+        assertFalse(myService.isMyPropertyVisible())
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
